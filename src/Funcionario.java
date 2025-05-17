7bde25c07691c693d37c9b664d3b609abdef48c2
sCesar-dev <samueljc003@gmail.com>
2025-05-16 22:38:56 -0300

    feat: Add Funcionario com seu construtor, getters, setters e metódos!

---
+++ src/Funcionario.java
@@ -0,0 +1,20 @@
+public class Funcionario extends Pessoa{
+    private String turnoTrab;
+
+    public Funcionario(String nome, String cpf, String email, long int telefone, String turnoTrab){
+        super(nome, cpf, email, telefone);
+        this.turnoTrab = turnoTrab;
+    }
+
+    public String getTurnoTrab() {
+        return turnoTrab;
+    }
+
+    public void setTurnoTrab(String turnoTrab) {
+        this.turnoTrab = turnoTrab;
+    }
+
+    public void excluirFunc(Funcionario funcionario) {
+        funcionario = null;
+    }
+}
