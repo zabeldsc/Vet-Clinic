301b641ae54386c22906018800fc670c1c4e405a
sCesar-dev <samueljc003@gmail.com>
2025-05-16 22:03:47 -0300

    feat: Add class Pessoa.java if constructor, getters and setters

---
+++ src/Pessoa.java
@@ -0,0 +1,44 @@
+class Pessoa{
+    private String nome, cpf, email;
+    private long int telefone;
+
+    public Pessoa(String nome, String cpf, String email, int telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+}
+
